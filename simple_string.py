d8a98104ea459710d65da1100c2e7ed8b3a4d903
felipefrancos <ffrancos.ieu2023@student.ie.edu>
2025-01-31 11:24:36 +0100

    strings

---
+++ simple_string.py
@@ -0,0 +1,12 @@
+s="hello"
+print(s)
+s2=("Hello shir")
+print(s2)
+s=('And she said hello')
+print(s)
+print(",.'\"\"\"\"")
+s="hello"
+print(s[2],s[4])# you indexby position, first index is 0
+print(s[-1])#go around the end
+print(s[1],s[-5])
+#print(s[11])
