5c4199bc304a1684a0358063be8b23dc2b1d4160
felipefrancos <ffrancos.ieu2023@student.ie.edu>
2025-01-31 11:05:58 +0100

    Added for examples

---
+++ Session 7-8.py
@@ -0,0 +1,14 @@
+for i in range (1,10,2):
+    print(i)
+s=0
+for i in range(1,11):
+    s+=i
+    #s=s+1
+print(s)
+
+i=1
+s=0
+while i<=10:
+    s+=i
+    i+=1
+print("sum using while is also", s)
